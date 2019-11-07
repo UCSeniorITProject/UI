8fa0871aaae814f44604dfcfd4a2c245ad77153b
Samuel Curry <currysl@ucmail.uc.edu>
2019-11-07 13:53:15 -0500

    refactoring more code

--- src/routes.js
+++ src/routes.js
@@ -7,34 +7,27 @@ const routes = [
   {
     collapse: true,
     name: "Pages",
-    rtlName: "صفحات",
     icon: "tim-icons icon-image-02",
     state: "pagesCollapse",
     views: [
       {
         path: "/login",
         name: "Login",
-        rtlName: "هعذاتسجيل الدخول",
         mini: "L",
-        rtlMini: "هعذا",
         component: Login,
         layout: "/auth"
       },
       {
         path: "/register",
         name: "Register",
-        rtlName: "تسجيل",
         mini: "R",
-        rtlMini: "صع",
         component: Register,
         layout: "/auth"
       },
       {
         path: "/user-profile",
         name: "User Profile",
-        rtlName: "ملف تعريفي للمستخدم",
         mini: "UP",
-        rtlMini: "شع",
         component: User,
         layout: "/admin"
       }
