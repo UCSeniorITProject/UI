37a2d02dad09fdf62c9a74bc0c930d062bf6d2ec
Samuel Curry <currysl@ucmail.uc.edu>
2019-11-08 09:27:14 -0500

    form validation stuff

--- src/views/pages/Register.jsx
+++ src/views/pages/Register.jsx
@@ -24,7 +24,8 @@ import {
 import ImageUpload from "components/CustomUpload/ImageUpload.jsx";
 
 class Register extends React.Component {
-  componentDidMount() {
+  constructor(props){
+    super(props);
     this.state = {
       username: '',
       password: '',
@@ -34,19 +35,148 @@ class Register extends React.Component {
       firstName: '',
       lastName: '',
       tos: false,
+      registerEmailState: '',
     };
+  }
+  componentDidMount() {
     document.body.classList.toggle("register-page");
   }
   componentWillUnmount() {
     document.body.classList.toggle("register-page");
   }
 
-  handleChange(e){
-    console.log({ [e.target.name]: e.target.value})
-		this.setState({ [e.target.name]: e.target.value});
+  handleChange(event, stateName, type, stateNameEqualTo, maxValue){
+    this.setState({ [event.target.name]: event.target.value});
+    switch (type) {
+      case "email":
+        if (this.verifyEmail(event.target.value)) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        console.log(this.state.registerEmailState)
+        break;
+      case "password":
+        if (this.verifyLength(event.target.value, 1)) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "equalTo":
+        if (this.compare(event.target.value, this.state[stateNameEqualTo])) {
+          this.setState({ [stateName + "State"]: "has-success" });
+          this.setState({ [stateNameEqualTo + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+          this.setState({ [stateNameEqualTo + "State"]: "has-danger" });
+        }
+        break;
+      case "number":
+        if (this.verifyNumber(event.target.value)) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "length":
+        if (this.verifyLength(event.target.value, stateNameEqualTo)) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "max-length":
+        if (!this.verifyLength(event.target.value, stateNameEqualTo + 1)) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "url":
+        if (this.verifyUrl(event.target.value)) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "min-value":
+        if (
+          this.verifyNumber(event.target.value) &&
+          event.target.value >= stateNameEqualTo
+        ) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "max-value":
+        if (
+          this.verifyNumber(event.target.value) &&
+          event.target.value <= stateNameEqualTo
+        ) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      case "range":
+        if (
+          this.verifyNumber(event.target.value) &&
+          event.target.value >= stateNameEqualTo &&
+          event.target.value <= maxValue
+        ) {
+          this.setState({ [stateName + "State"]: "has-success" });
+        } else {
+          this.setState({ [stateName + "State"]: "has-danger" });
+        }
+        break;
+      default:
+        break;
+    }
   }
 
-  handleImageChange(imageUrl){
+  // function that returns true if value is email, false otherwise
+  verifyEmail = value => {
+    var emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRex.test(value)) {
+      return true;
+    }
+    return false;
+  };
+  // function that verifies if a string has a given length or not
+  verifyLength = (value, length) => {
+    if (value.length >= length) {
+      return true;
+    }
+    return false;
+  };
+  // function that verifies if two strings are equal
+  compare = (string1, string2) => {
+    if (string1 === string2) {
+      return true;
+    }
+    return false;
+  };
+  // function that verifies if value contains only numbers
+  verifyNumber = value => {
+    var numberRex = new RegExp("^[0-9]+$");
+    if (numberRex.test(value)) {
+      return true;
+    }
+    return false;
+  };
+  // verifies if value is a valid URL
+  verifyUrl = value => {
+    try {
+      new URL(value);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  };
+
+  handleImageChange = imageUrl => {
     this.setState({profilePicture: imageUrl});
   }
 
@@ -136,13 +266,13 @@ class Register extends React.Component {
                         </InputGroupAddon>
                         <Input placeholder="Last name" name="lastName" type="text" onChange={this.handleChange.bind(this)}/>
                       </InputGroup>
-                      <InputGroup>
+                      <InputGroup className={`has-label ${this.state.registerEmailState}`}>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="tim-icons icon-email-85" />
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input placeholder="Email" name="email" type="text" onChange={this.handleChange.bind(this)}/>
+                        <Input placeholder="Email" name="email" type="email" onChange={e => this.handleChange(e, "registerEmail", "email")}/>
                       </InputGroup>
                       <InputGroup>
                         <InputGroupAddon addonType="prepend">
