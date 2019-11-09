86c4bffde3d5928d94742c7d7d9f6b210e66b7a3
Samuel Curry <currysl@ucmail.uc.edu>
2019-11-08 21:38:22 -0500

    some validation stuff

--- src/views/pages/Register.jsx
+++ src/views/pages/Register.jsx
@@ -22,6 +22,7 @@ import {
   Col
 } from "reactstrap";
 import ImageUpload from "components/CustomUpload/ImageUpload.jsx";
+import NotificationAlert from "react-notification-alert";
 
 class Register extends React.Component {
   constructor(props){
@@ -36,6 +37,8 @@ class Register extends React.Component {
       lastName: '',
       tos: false,
       registerEmailState: '',
+      registerPhoneNumberState: '',
+      registerPasswordState: '',
     };
   }
   componentDidMount() {
@@ -43,6 +46,24 @@ class Register extends React.Component {
   }
   componentWillUnmount() {
     document.body.classList.toggle("register-page");
+  }
+
+  alertUserOfPasswordRequirements(){
+    var options = {};
+    options = {
+      place: 'tr',
+      message: (
+        <div>
+          <div>
+            Your password must be 6 characters or more and include one capital later.
+          </div>
+        </div>
+      ),
+      type: 'warning',
+      icon: "tim-icons icon-bell-55",
+      autoDismiss: 7
+    };
+    this.refs.notificationAlert.notificationAlert(options);
   }
 
   handleChange(event, stateName, type, stateNameEqualTo, maxValue){
@@ -54,78 +75,24 @@ class Register extends React.Component {
         } else {
           this.setState({ [stateName + "State"]: "has-danger" });
         }
-        console.log(this.state.registerEmailState)
         break;
       case "password":
-        if (this.verifyLength(event.target.value, 1)) {
+        if (this.verifyLength(event.target.value, 6)
+         && event.target.value.toLowerCase() !== event.target.value) {
           this.setState({ [stateName + "State"]: "has-success" });
         } else {
           this.setState({ [stateName + "State"]: "has-danger" });
         }
         break;
-      case "equalTo":
-        if (this.compare(event.target.value, this.state[stateNameEqualTo])) {
-          this.setState({ [stateName + "State"]: "has-success" });
-          this.setState({ [stateNameEqualTo + "State"]: "has-success" });
-        } else {
-          this.setState({ [stateName + "State"]: "has-danger" });
-          this.setState({ [stateNameEqualTo + "State"]: "has-danger" });
-        }
-        break;
-      case "number":
-        if (this.verifyNumber(event.target.value)) {
+      case "tel":
+        if(this.verifyPhone(event.target.value)){
           this.setState({ [stateName + "State"]: "has-success" });
         } else {
           this.setState({ [stateName + "State"]: "has-danger" });
         }
         break;
-      case "length":
-        if (this.verifyLength(event.target.value, stateNameEqualTo)) {
-          this.setState({ [stateName + "State"]: "has-success" });
-        } else {
-          this.setState({ [stateName + "State"]: "has-danger" });
-        }
-        break;
-      case "max-length":
-        if (!this.verifyLength(event.target.value, stateNameEqualTo + 1)) {
-          this.setState({ [stateName + "State"]: "has-success" });
-        } else {
-          this.setState({ [stateName + "State"]: "has-danger" });
-        }
-        break;
-      case "url":
-        if (this.verifyUrl(event.target.value)) {
-          this.setState({ [stateName + "State"]: "has-success" });
-        } else {
-          this.setState({ [stateName + "State"]: "has-danger" });
-        }
-        break;
-      case "min-value":
-        if (
-          this.verifyNumber(event.target.value) &&
-          event.target.value >= stateNameEqualTo
-        ) {
-          this.setState({ [stateName + "State"]: "has-success" });
-        } else {
-          this.setState({ [stateName + "State"]: "has-danger" });
-        }
-        break;
-      case "max-value":
-        if (
-          this.verifyNumber(event.target.value) &&
-          event.target.value <= stateNameEqualTo
-        ) {
-          this.setState({ [stateName + "State"]: "has-success" });
-        } else {
-          this.setState({ [stateName + "State"]: "has-danger" });
-        }
-        break;
-      case "range":
-        if (
-          this.verifyNumber(event.target.value) &&
-          event.target.value >= stateNameEqualTo &&
-          event.target.value <= maxValue
-        ) {
+      case "length": 
+        if(this.verifyPhone(event.target.length, 1)){
           this.setState({ [stateName + "State"]: "has-success" });
         } else {
           this.setState({ [stateName + "State"]: "has-danger" });
@@ -136,9 +103,16 @@ class Register extends React.Component {
     }
   }
 
+  verifyPhone = value => {
+    const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+    if(phoneRegex.test(value)){
+      return true;
+    }
+    return false;
+  };
   // function that returns true if value is email, false otherwise
   verifyEmail = value => {
-    var emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (emailRex.test(value)) {
       return true;
     }
@@ -151,30 +125,8 @@ class Register extends React.Component {
     }
     return false;
   };
-  // function that verifies if two strings are equal
-  compare = (string1, string2) => {
-    if (string1 === string2) {
-      return true;
-    }
-    return false;
-  };
-  // function that verifies if value contains only numbers
-  verifyNumber = value => {
-    var numberRex = new RegExp("^[0-9]+$");
-    if (numberRex.test(value)) {
-      return true;
-    }
-    return false;
-  };
-  // verifies if value is a valid URL
-  verifyUrl = value => {
-    try {
-      new URL(value);
-      return true;
-    } catch (_) {
-      return false;
-    }
-  };
+
+
 
   handleImageChange = imageUrl => {
     this.setState({profilePicture: imageUrl});
@@ -183,6 +135,9 @@ class Register extends React.Component {
   render() {
     return (
       <>
+        <div className="rna-container">
+          <NotificationAlert ref="notificationAlert" />
+        </div>
         <div className="content">
           <Container>
             <Row>
@@ -258,7 +213,6 @@ class Register extends React.Component {
                         <Input placeholder="First Name" name="firstName" type="text" onChange={this.handleChange.bind(this)}/>
                       </InputGroup>
                       <InputGroup>
-                      <InputGroup>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="tim-icons icon-single-02" />
@@ -266,10 +220,10 @@ class Register extends React.Component {
                         </InputGroupAddon>
                         <Input placeholder="Last name" name="lastName" type="text" onChange={this.handleChange.bind(this)}/>
                       </InputGroup>
-                      <InputGroup className={`has-label ${this.state.registerEmailState}`}>
+                      <InputGroup className={`has-label`}>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
-                            <i className="tim-icons icon-email-85" />
+                            <i className={`tim-icons icon-email-85 ${this.state.registerEmailState}`} />
                           </InputGroupText>
                         </InputGroupAddon>
                         <Input placeholder="Email" name="email" type="email" onChange={e => this.handleChange(e, "registerEmail", "email")}/>
@@ -277,21 +231,21 @@ class Register extends React.Component {
                       <InputGroup>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
-                            <i className="tim-icons icon-lock-circle" />
+                            <i className={`tim-icons icon-lock-circle ${this.state.registerPasswordState}`} />
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input placeholder="Password" name="password" type="password" onChange={this.handleChange.bind(this)}/>
+                        <Input onClick={e => this.alertUserOfPasswordRequirements()}placeholder="Password" name="password" type="password" onChange={e => this.handleChange(e, "registerPassword", "password")}/>
                       </InputGroup>
+                      <InputGroup>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
-                            <i className="tim-icons icon-chat-33" />
+                            <i className={`tim-icons icon-chat-33 ${this.state.registerPhoneNumberState}`} />
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input placeholder="Phone Number" name="phoneNumber" type="tel" onChange={this.handleChange.bind(this)}/>
+                        <Input placeholder="Phone Number" name="phoneNumber" type="text" onChange={e => this.handleChange(e, "registerPhoneNumber", "tel")}/>
                       </InputGroup>
                       <FormGroup check className="text-left">
                         <Label check>
-                          <Input type="checkbox" name="tos" onChange={this.handleChange.bind(this)}/>
                           <span className="form-check-sign" />I agree to the{" "}
                             terms and conditions.
                         </Label>
