60a3f7119a8ba3c8155c8d915135f3ec09dbbbae
Samuel Curry <currysl@ucmail.uc.edu>
2019-11-18 22:59:01 -0500

    empty page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Register from "views/pages/Register.jsx";
 
+import Prescribe from "views/pages/Prescribe.jsx";
 import User from "views/pages/User.jsx";
 import Login from "views/pages/Login.jsx";
 
@@ -16,7 +17,6 @@ const routes = [
         name: "User Profile",
         rtlName: "ملف تعريفي للمستخدم",
         mini: "UP",
-        rtlMini: "شع",
         component: User,
         isHidden: false,
         layout: "/admin"
@@ -39,6 +39,23 @@ const routes = [
       },
     ]
   },
+  {
+    collapse: true,
+    name: "Pharmacy",
+    rtlName: "tim-icons icon-tap-02",
+    icon: "tim-icons icon-tap-02",
+    state: "pagesCollapse",
+    views: [
+      {
+        path: "/prescribe",
+        name: "Prescribe",
+        mini: "P",
+        component: Prescribe,
+        isHidden: false,
+        layout: "/admin"
+      },
+    ],
+  }
 ];
 
 export default routes;
---
+++ src/views/pages/Prescribe.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+class Prescribe extends React.Component {
+
+}
+
+export default Prescribe;
