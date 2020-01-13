fe198a9b213111c8acfe42587d50eb511be614d5
Samuel Curry <currysl@ucmail.uc.edu>
2020-01-13 17:08:00 -0500

    adjusted axios interceptor

--- src/services/SetupAxiosInterceptors.js
+++ src/services/SetupAxiosInterceptors.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import {refreshAccessToken} from '../services/User';
 export default async (store, history) => {
       //does not matter if token is null -- this makes routes that require no token easier
       axios.interceptors.request.use(async request => {
@@ -16,14 +16,10 @@ export default async (store, history) => {
           const refreshToken = localStorage.getItem("refreshToken");
           if(refreshToken){
             try {
-              const refreshedTokens = await axios.post(`${process.env.REACT_APP_API_URL}/api/security-management/user/token/refresh`,
-                {
-                  refreshToken,
-                }
-              );
-              localStorage.setItem('accessToken', refreshedTokens.accessToken);
-              localStorage.setItem('refreshToken', refreshedTokens.refreshToken);
-              error.response.config.headers['Authorization'] = `Bearer ${refreshedTokens.accessToken}`;
+              const tokens = refreshAccessToken(refreshAccessToken)
+              localStorage.setItem('accessToken', tokens.accessToken);
+              localStorage.setItem('refreshToken', tokens.refreshToken);
+              error.response.config.headers['Authorization'] = `Bearer ${tokens.accessToken}`;
               return axios(error.response.config);
             } catch (err) {
               history.push('/auth/login');
