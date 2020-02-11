cdc6712fe98e8b7798fdf3b48b8c05c95cc53d2a
Samuel Curry <samc735@gmail.com>
2020-02-11 00:01:27 -0500

    added web.config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import PrivateRoute from './components/DecisionRoute/DecisionRoute';
+import "./web.config";
 import AuthLayout from "./layouts/Auth/Auth.jsx";
 import AdminLayout from "./layouts/Admin/Admin.jsx";
 import {refreshAccessToken} from './services/User';
