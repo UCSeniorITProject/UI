433d9e9bc3f88d56feef45dc156d98498ab6d4c4
Samuel Curry <samc735@gmail.com>
2020-03-13 00:31:00 -0400

    Update ChoosePharmacy.jsx

--- src/views/pages/PrescribeSteps/ChoosePharmacy.jsx
+++ src/views/pages/PrescribeSteps/ChoosePharmacy.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { getPharmacyWithFilter } from "../../../services/Pharmacy";
 import ReactTable from "react-table";
-
+import {
+  Button,
+  Row,
+  Col
+} from "reactstrap";
 class ChoosePharmacy extends React.Component {
   constructor(props){
     super(props);
@@ -23,7 +27,66 @@ class ChoosePharmacy extends React.Component {
   render(){
     return (
       <>
-      </>
+				<div className="rna-container">
+					<NotificationAlert ref="notificationAlert" />
+				</div>
+				<Row>
+					<Col md="9">
+					<h5 className="info-text float-left">
+							<b>Patient Picker (Currently Selected Patient: {this.state.currentlySelectedPatientName})</b>
+						</h5>
+					</Col>
+					<Col md="3">
+						<Button
+								className="animation-on-hover float-right"
+								color="success"
+								id="addPatient"
+								type="button"
+								onClick={e => this.props.history.push('/admin/patient/new')}
+							>
+								Add Patient
+							</Button>
+					</Col>
+				</Row>
+
+					<ReactTable
+											data={this.state.patients}
+											filterable
+											resizable={false}
+											columns={[
+												{
+													Header: "Patient ID",
+													accessor: "patientId",
+												},
+												{
+													Header: "First Name",
+													accessor: "firstName"
+												},
+												{
+													Header: "Last Name",
+													accessor: "lastName"
+												},
+												{
+													Header: "Date of Birth",
+													accessor: "dob"
+												},
+												{
+													Header: "Gender",
+													accessor: "gender"
+												},
+												{
+													Header: "Actions",
+													accessor: "actions",
+													sortable: false,
+													filterable: false
+												}
+											]}
+											defaultPageSize={5}
+											showPaginationTop
+											showPaginationBottom={false}
+											className="-striped -highlight"
+										/>
+				</>
     )
   }
 }
